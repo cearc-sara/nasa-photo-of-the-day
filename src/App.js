40a872ac63dd2f1e4a77da49deda5ee53b59c890
Sara Cearc <cearc-sara@lambdastudents.com>
2020-08-12 15:43:23 -0400

    added components and wireframe to project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import Header from "./Components/header"
+import Footer from "./Components/footer"
+import Images from "./Photos/images"
+import Text from "./Text/text"
+
 import "./App.css";
 
 function App() {
+
+
+
+
+
+
+  
   return (
     <div className="App">
+      <Header />
+      <Footer />
+      <Images />
+      <Text />
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
